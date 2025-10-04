d2d7491fcc6c247aa533de3399f174e2a6fc463f
myamusashi <myamusashi@proton.me>
2025-10-04 21:38:28 +0700

    database: Fix AUTO_INCREMENT, fix and remove unnecessary Table

--- database/db_schema.sql
+++ database/db_schema.sql
@@ -1,16 +1,14 @@
 CREATE TABLE Pasien (
-    id_pasien INT PRIMARY KEY AUTO_INCREMENT,
-	nama_depan VARCHAR(75) NOT NULL,
-	nama_belakang VARCHAR(75) NOT NULL,
+    id_pasien SERIAL PRIMARY KEY,
     username VARCHAR(75) NOT NULL UNIQUE,
     email VARCHAR(254) NOT NULL UNIQUE,
-	umur DATE NOT NULL,
+    tanggal_lahir DATE NOT NULL,
     password TEXT NOT NULL,
     tinggi_badan FLOAT NOT NULL,
-	berat_badan FLOAT NOT NULL,
-	jenis_kelamin ENUM('Laki-laki', 'Perempuan'),
-	alamat VARCHAR(255),
-	nomor_telepon CHAR(20),
+    berat_badan FLOAT NOT NULL,
+    jenis_kelamin VARCHAR(10) CHECK (jenis_kelamin IN ('Laki-laki', 'Perempuan')) NOT NULL,
+    alamat VARCHAR(255),
+    nomor_telepon CHAR(20),
     is_admin BOOLEAN NOT NULL DEFAULT FALSE,
     is_active BOOLEAN NOT NULL DEFAULT TRUE,
     last_login_at TIMESTAMP,
@@ -18,47 +16,17 @@ CREATE TABLE Pasien (
     updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- Auto update 'update_at'
+-- Auto update 'updated_at'
 CREATE OR REPLACE FUNCTION updated_at_column()
 RETURNS TRIGGER AS $$
 BEGIN
-	NEW.update_at = CURRENT_TIMESTAMP;
-	RETURN NEW;
+    NEW.updated_at = CURRENT_TIMESTAMP;
+    RETURN NEW;
 END;
-$$ LANGUAGE plpgqsl;
+$$ LANGUAGE plpgsql;
 
-CREATE TRIGGER update_patient_updated_at
-	BEFORE UPDATE ON Pasien
-	FOR EACH ROW
-	EXECUTE FUNCTION updated_at_column();
+CREATE TRIGGER update_pasien_updated_at
+    BEFORE UPDATE ON Pasien
+    FOR EACH ROW
+    EXECUTE FUNCTION updated_at_column();
 
-CREATE TABLE Kondisi_pasien (
-	id_kondisi INT PRIMARY KEY AUTO_INCREMENT,
-	id_pasien INT,
-	waktu_pengukuran DATETIME NOT NULL,
-	detak_jantung INT,
-	kadar_gula DECIMAL(4, 2),
-	temperatur DECIMAL(4, 2),
-	FOREIGN KEY(id_pasien) REFERENCES Pasien(id_pasien)
-);
-
-CREATE TABLE Catatan_medis (
-	id_catatan INT PRIMARY KEY AUTO_INCREMENT,
-	id_pasien INT,
-	waktu_pencatatan DATE NOT NULL,
-	diagnosis TEXT,
-	perawatan TEXT,
-	catatan TEXT,
-	FOREIGN KEY(id_pasien) REFERENCES Pasien(id_pasien)
-);
-
-CREATE TABLE Pengobatan (
-	id_pengobatan INT PRIMARY KEY AUTO_INCREMENT,
-	id_pasien INT,
-	nama_pengobatan VARCHAR(150) NOT NULL,
-	dosis VARCHAR(50),
-	frekuensi VARCHAR(50),
-	start_date DATE,
-	end_date DATE,
-	FOREIGN KEY(id_pasien) REFERENCES Pasien(id_pasien)
-);
